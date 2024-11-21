a78165a0059ed0224ed5b82af37b0e317fa0a83b
edelagnier <erica.delagnier@multi.coop>
2024-11-21 18:27:07 +0100

    Revert "front"
    This reverts commit d0bd3f20e5adf4a423e5bd938263b2593bf1e390.

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -154,7 +154,7 @@
 			"mon2.agatha-budget.fr" = {
 				forceSSL = true;
 				enableACME = true;
-				root = "/var/www/front/dist";
+				root = "/var/www/front/";
 				locations."/" = {
 					tryFiles = "$uri $uri/ /index.html"; 
 				};
@@ -162,7 +162,7 @@
 			"beta2.agatha-budget.fr" = {
 				forceSSL = true;
 				enableACME = true;
-				root = "/var/www/beta/dist";
+				root = "/var/www/beta/";
 				locations."/" = {
 					tryFiles = "$uri $uri/ /index.html"; # redirect subpages url
 				};
