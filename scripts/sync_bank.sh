ec4d94ba16d50875e35f7cc0db29d9341a33ac39
edelagnier <erica.delagnier@multi.coop>
2024-11-28 15:06:41 +0100

    use api2

--- scripts/sync_bank.sh
+++ scripts/sync_bank.sh
@@ -1,1 +1,1 @@
-curl -X 'POST'   'https://betapi.agatha-budget.fr/bank/sync'   -H 'accept: application/json'   -d ''
+curl -X 'POST'   'https://betapi2.agatha-budget.fr/bank/sync'   -H 'accept: application/json'   -d ''
