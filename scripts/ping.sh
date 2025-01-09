8503678aed8ae1f5a341e399e3dd395d37824b63
edelagnier <erica.delagnier@multi.coop>
2025-01-09 16:16:13 +0100

    improve

--- scripts/ping.sh
+++ scripts/ping.sh
@@ -1,13 +1,13 @@
-timestamp=$(date +"%Y-%m-%d-%H-%M-%S")_backup
+timestamp=$(date +"%Y-%m-%d-%H-%M-%S")
 my=$(curl --silent --output /dev/null --write-out "%{http_code}" -X 'GET'   'https://betapi2.agatha-budget.fr/ping'   -H 'accept: application/json'   -d '')
 echo ${my}
 
 case ${my} in 
     *200*)
-        echo $(timestamp)_RAS_beta >> /home/erica/ping.txt
+        echo ${timestamp}_RAS_beta >> /home/erica/ping.txt
         ;;
     *)
-        echo $(timestamp)_PROBLEM_beta >> /home/erica/ping.txt
+        echo ${timestamp}_PROBLEM_beta >> /home/erica/ping.txt
         systemctl restart betabackend
         ;;
 esac
@@ -17,10 +17,10 @@ echo ${my}
 
 case ${my} in 
     *200*)
-        echo $(timestamp)_RAS_default >> /home/erica/ping.txt
+        echo ${timestamp}_RAS_default >> /home/erica/ping.txt
         ;;
     *)
-        echo $(timestamp)_PROBLEM_default >> /home/erica/ping.txt
+        echo ${timestamp}_PROBLEM_default >> /home/erica/ping.txt
         systemctl restart backend
         ;;
 esac
